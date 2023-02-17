4abc1c4de76a5c355f26e6ade943e71d69f02fd7
helenisagenius <oluwatomisinjegede525@gmail.com>
2023-02-17 11:43:45 +0000

    creative minds

--- light.java
+++ light.java
@@ -2,5 +2,6 @@
 # please do not hard code
 # the use of variables is highly requested
 # both dynamic or static variable are aceptable
+# new ideas, great innovation and creative minds.
 # Author: oluwatomisin
 date
